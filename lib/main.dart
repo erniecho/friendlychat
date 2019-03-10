3ef6c0d2c9686be346b681b908364f0f8f29a4d3
erniecho <erniecho@gmail.com>
2019-03-10 17:30:54 -0400

    added make the send button context-aware

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 
 const String _name = "Your Name";
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 void main() {
   runApp(new FriendlychatApp());
 }
@@ -10,6 +22,9 @@ class FriendlychatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Friendlychat",
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+        ? kIOSTheme
+        : kDefaultTheme,
       home: new ChatScreen(),
     );
   }
@@ -23,6 +38,7 @@ class ChatScreen extends StatefulWidget {
 class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final List<ChatMessage> _messages = <ChatMessage>[]; //Message Array.
   final TextEditingController _textController = new TextEditingController();
+  bool _isComposing = false;
   @override
   void dispose() {
     for (ChatMessage message in _messages)
@@ -31,25 +47,36 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   }
   Widget build(BuildContext context) {
     return new Scaffold (
-      appBar: new AppBar(title: new Text("Friendlychat")),
-      body: new Column(
-        children: <Widget>[
-          new Flexible(
-            child: new ListView.builder(
-              padding: new EdgeInsets.all(8.0),
-              reverse: true,
-              itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length,
+      appBar: new AppBar(
+          title: new Text("Friendlychat"),
+          elevation: new Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+    ),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new Flexible(
+              child: new ListView.builder(
+                padding: new EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              ),
             ),
+            new Divider(height: 1.0),
+            new Container(
+              decoration: new BoxDecoration(
+                color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            ),
+          ],
+        ),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+        ? new BoxDecoration(
+          border: new Border(
+            top: new BorderSide(color: Colors.grey[200]),
           ),
-          new Divider(height: 1.0),
-          new Container(
-            decoration: new BoxDecoration(
-              color: Theme.of(context).cardColor),
-            child: _buildTextComposer(),
-          ),
-        ],
-      ),
+        )
+      : null),
     );
   }
 
@@ -61,6 +88,11 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
           new Flexible(                                          //new
             child: new TextField(
               controller: _textController,
+              onChanged: (String text) {
+                setState(() {
+                  _isComposing = text.length > 0;
+                });
+              },
               onSubmitted: _handleSubmitted,
               decoration: new InputDecoration.collapsed(
                   hintText: "Send a message"),
@@ -68,9 +100,17 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
           ),                                                      //new
           new Container(
             margin: new EdgeInsets.symmetric(horizontal: 4.0),
-            child: new IconButton(
+            child: Theme.of(context).platform == TargetPlatform.iOS ?
+                new CupertinoButton(
+                  child: new Text("Send"),
+                  onPressed: _isComposing
+                  ? () => _handleSubmitted(_textController.text)
+                      : null,) :
+            new IconButton(
               icon: new Icon(Icons.send),
-              onPressed: () => _handleSubmitted(_textController.text)
+              onPressed: _isComposing
+                ? () => _handleSubmitted(_textController.text)
+                  : null,
             ),
           ),
         ],                                                        //new
@@ -80,6 +120,9 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    setState ((){
+      _isComposing = false;
+    });
     ChatMessage message = new ChatMessage(
       text: text,
       animationController: new AnimationController(
@@ -115,15 +158,17 @@ class ChatMessage extends StatelessWidget {
                 margin: const EdgeInsets.only(right: 16.0),
                 child: new CircleAvatar(child: new Text(_name[0])),
               ),
-              new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new Text(_name, style: Theme.of(context).textTheme.subhead),
-                  new Container(
-                    margin: const EdgeInsets.only(top: 5.0),
-                    child: new Text(text),
-                  ),
-                ],
+              new Expanded(
+                child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Text(_name, style: Theme.of(context).textTheme.subhead),
+                    new Container(
+                      margin: const EdgeInsets.only(top: 5.0),
+                      child: new Text(text),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
